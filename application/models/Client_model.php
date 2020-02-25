10fbe524f8157317adaf40f0b31709d1eef8b5d4
caroline-ctrl <duon.caroline@gmail.com>
2020-02-25 21:38:52 +0100

    page Client_model remplie

--- application/models/Client_model.php
+++ application/models/Client_model.php
@@ -2,5 +2,20 @@
 
 class Client_model extends CI_Model
 {
-    
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+
+    public function get_client($slug = FALSE)
+    {
+        if ($slug === FALSE){
+            $query = $this->db->get('client');
+            return $query->result_array();
+        }else {
+            $query = $this->db->get_where('client', array('slug' => $slug));
+            return $query->row_array();
+        }
+    }
 }
